5c667c482b9fe34db4685b98e58369d9ebb45fae
jamochl <james.lim@jamochl.com>
2021-09-16 12:28:02 +1000

    add public ip and role arn into output

---
+++ outputs.tf
@@ -0,0 +1,10 @@
+output "ec2_public_ip" {
+  value = "ssh ec2-user@${aws_instance.bastion.public_ip}"
+}
+
+output "role_arns" {
+  value = {
+    ec2_to_assume_arn = aws_iam_role.ec2_to_assume.arn
+    assume_to_s3_arn = aws_iam_role.assume_to_s3.arn
+  }
+}
