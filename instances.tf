0743433fa1edabf060f088777d4595435665e125
jamochl <james.lim@jamochl.com>
2021-09-16 12:00:45 +1000

    instances and vpc in working state

--- instances.tf
+++ instances.tf
@@ -5,3 +5,23 @@ resource "aws_key_pair" "deployer" {
     Name = "deployer key"
   }
 }
+
+data "aws_ssm_parameter" "amazon-linux-2" {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
+
+resource "aws_instance" "web" {
+  ami                         = data.aws_ssm_parameter.amazon-linux-2.value
+  instance_type               = "t3a.micro"
+  key_name                    = aws_key_pair.deployer.key_name
+  associate_public_ip_address = true
+  subnet_id                   = data.aws_subnet.selected.id
+
+  vpc_security_group_ids = [
+    aws_security_group.allow_ssh.id
+  ]
+
+  tags = {
+    Name = "bastion host"
+  }
+}
--- vpc.tf
+++ vpc.tf
@@ -3,6 +3,7 @@ data "aws_vpc" "selected" {
   default = true
 }
 
-data "aws_subnet_ids" "selected" {
+data "aws_subnet" "selected" {
   vpc_id = data.aws_vpc.selected.id
+  availability_zone = "us-east-1a"
 }
