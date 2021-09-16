77c164d75c4df836231f21820c817dee0c9070f6
jamochl <james.lim@jamochl.com>
2021-09-16 11:10:02 +1000

    roles in working state

--- roles.tf
+++ roles.tf
@@ -1,17 +1,73 @@
 resource "aws_iam_role" "ec2_to_assume" {
-  name = "ec2_to_s3"
+  name = "ec2_to_assume_role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Sid    = "Allow EC2 to assume this role"
         Effect = "Allow"
         Action = "sts:AssumeRole"
-        Principle = {
+        Principal = {
           Service = "ec2.amazonaws.com"
         }
       },
+    ]
+  })
+
+  tags = {
+    Name = "ec2_to_assume_role"
+  }
+}
+
+resource "aws_iam_role_policy" "ec2_to_assume" {
+  name = "ec2_to_assume_policy"
+
+  role = aws_iam_role.ec2_to_assume.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "sts:AssumeRole"
+        Resource = aws_iam_role.assume_to_s3.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "assume_to_s3" {
+  name = "assume_to_s3_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "sts:AssumeRole"
+        Principal = {
+          AWS = aws_iam_role.ec2_to_assume.arn
+        }
+      },
+    ]
+  })
+
+  tags = {
+    Name = "assume_to_s3_role"
+  }
+}
+
+resource "aws_iam_role_policy" "assume_to_s3" {
+  name = "assume_to_s3_policy"
+
+  role = aws_iam_role.assume_to_s3.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "s3:*"
+        Resource = "*"
+      }
     ]
   })
 }
